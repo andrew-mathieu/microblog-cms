00bb80f3250dac2a3b8474f455d1af791b3cac8a
Andrew Mathieu <jesztarmgmt@gmail.com>
2023-10-11 02:15:12 +0200

    update page.tsx

--- app/admin/post/new/page.tsx
+++ app/admin/post/new/page.tsx
@@ -6,7 +6,7 @@ import {
   useEffect,
   TextareaHTMLAttributes,
 } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import PocketBase from "pocketbase";
 import type * as pb from "@/types/pocketbase-types";
 import Card from "@/components/ui/Card";
